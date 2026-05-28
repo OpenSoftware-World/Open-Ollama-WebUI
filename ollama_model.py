a5427df337e79f1ec7dcb3e3a187f66de0638bcf
OpenSoftware-World <143949134+OpenSoftware-World@users.noreply.github.com>
2026-05-28 18:04:16 +0300

    To enable you to chat with the AI model you use with Ollama, Ollama’s official Python library is now being used.

--- ollama_model.py
+++ ollama_model.py
@@ -1,14 +1,10 @@
-#from ollama import ChatResponse, chat
-import subprocess
+from ollama import ChatResponse, chat
 def load_model(model_name, msg):
-    """messages = [
+    messages = [
         {
             "role": "user",
             "content": msg
         },
     ]
     response: ChatResponse = chat(model=model_name, messages=messages)
-    return response.message.content"""
-    model_response = subprocess.run(["ollama", "run", model_name, msg], capture_output=True, text=True, encoding="utf-8")
-    model_response = model_response.stdout.strip()
-    return model_response
+    return response.message.content
